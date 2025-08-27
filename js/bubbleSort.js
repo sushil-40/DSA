c0931c1a21478f8a2de1084d453d096ab8e970cc
Sushil <sushildangoriya40@gmail.com>
2025-08-27 19:53:07 +1000

    bubble sort completed

---
+++ js/bubbleSort.js
@@ -0,0 +1,17 @@
+let arrTest = [1, 5, 3, 2, 6, 12, 9];
+function bubbleSort(arr) {
+  let n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+let result = bubbleSort(arrTest);
+console.log(result);
